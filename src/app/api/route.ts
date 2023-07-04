9ad6a537e43e5e51c5047ff872b9979aa8c21395
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-07-04 18:09:41 +0300

    add delete code addition

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,29 +1,4 @@
-// import {NextResponse } from "next/server";
-// import {getDatabase,ref,set} from "firebase/database"
-// import { initializeApp } from "firebase/app";
 
-// const fierbaseConfig = {
-//     databaseURL: "https://todo-list-96653-default-rtdb.firebaseio.com/"
-// }
-
-// export async function GET(request:Request){
-//     const app = initializeApp(fierbaseConfig)
-//     const database= getDatabase(app)
-//     set(ref(database, "users/" + 1),{
-//       username: "yashar",
-//       gmail: "yasharnajafi6846@gmail.com",
-//       profile_name: "yashar",
-//     })
-//     return NextResponse.json({hello: "world"})
-// }
-// export async function POST(request: Request){
-//   const body = await request.json()
-//   const newItemRef = push(ref)
-//   set(newItemRef, body).then(()=>console.log("Done",newItemRef.key))
-//     return NextResponse.json({...body, key: newItemRef.key})
-// }
-
-//FierBase
 import { NextResponse } from "next/server";
 import {
   getDatabase,
@@ -39,7 +14,7 @@ import {
 import firebase, { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  databaseURL: "https://im-donkey-default-rtdb.firebaseio.com/",
+  databaseURL: "https://nexttypescript-default-rtdb.firebaseio.com",
 };
 
 
@@ -65,3 +40,4 @@ export async function POST(request: Request) {
   set(newItemRef, body).then(() => console.log("Done:", newItemRef.key));
   return NextResponse.json({ ...body, key: newItemRef.key });
 }
+
